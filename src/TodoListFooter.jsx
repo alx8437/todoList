88bb054da17bbb4dfd1e13287e668958d108b1ce
alx8437 <alx8437@mail.ru>
2020-04-29 23:21:13 +0300

    end 10 task class_work

--- src/TodoListFooter.jsx
+++ src/TodoListFooter.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 class TodoListFooter extends React.Component {
 
     state = {
-        isHidden: false
-    }
+        isHiden: true
+    };
 
 
+    onAllFilterClick = () => {this.props.changeFilter("All")};
+    onCompletedFilterClick = () => {this.props.changeFilter("Completed")}
+    onActiveFilterClick = () => {this.props.changeFilter("Active")};
+    onShowFiltersClick = () => {this.setState({isHiden: false})};
+    onHideFiltersClick = () => {this.setState({isHiden: true})};
 
     render = () => {
-
 
         let classForAll = this.props.filterValue === "All" ? "filter-active" : "";
         let classForCompleted = this.props.filterValue === "Completed" ? "filter-active" : "";
@@ -17,23 +21,23 @@ class TodoListFooter extends React.Component {
 
         return (
             <div className="todoList-footer">
-                {this.state.isHidden && <div>
+                {this.state.isHiden && <div>
                     <button
                         className={classForAll}
-                        onClick={()=>{this.props.changeFilter("All")}}
+                        onClick={this.onAllFilterClick}
                     >All</button>
                     <button
                         className={classForCompleted}
-                        onClick={()=>{this.props.changeFilter("Completed")}}
+                        onClick={this.onCompletedFilterClick}
                     >Completed</button>
                     <button
                         className={classForActive}
-                        onClick={()=>this.props.changeFilter("Active")}
+                        onClick={this.onActiveFilterClick}
                     >Active
                     </button>
                 </div>}
-                <span onClick={() => {this.setState({isHiden: false})}}>hide</span>
-                <span onClick={() => {this.setState({isHiden: true})}}>show</span>
+                <span className="hideFooterButton" onClick={this.onHideFiltersClick}>show</span>
+                <span className="hideFooterButton" onClick={this.onShowFiltersClick}>hide</span>
             </div>
 
         )
--- src/TodoListHeader.jsx
+++ src/TodoListHeader.jsx
@@ -6,23 +6,40 @@ class TodoListHeader extends React.Component {
         this.newTaskRef = React.createRef()
     }
 
+    state = {
+      error: false
+    };
+
+
     onAddTaskList = () => {
-        let newText = this.newTaskRef.current.value;
-        this.props.addTaskList(newText);
-        this.newTaskRef.current.value = '';
+        let newText = this.newTaskRef.current.value.trim();
+        if (newText === "") {
+            this.setState({
+                error: true
+            })
+        } else {
+            this.props.addTaskList(newText);
+            this.newTaskRef.current.value = '';
+        }
+
     };
 
 
 
     render = () => {
+
+        let classToError = this.state.error === true ? "error" : "";
+
         return (
             <div className="todoList-header">
                 <h3 className="todoList-header__title">What to Learn</h3>
                 <div className="todoList-newTaskForm">
                     <input
+                        className={classToError}
                         type="text"
                         placeholder="New task name"
                         ref={this.newTaskRef}
+
                     />
                     <button
                         onClick={this.onAddTaskList}
