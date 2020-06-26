9d82b0477007df9ef0d97eddd114e288c6ad7472
Alx8437 <alx8437@mail.ru>
2020-06-26 14:27:54 +0300

    Updates

---
+++ precache-manifest.366a970d268c945170d7a3d0c5a20e26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a2dfcc3c031d1f5e04792b034456f7",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "af4518a8be364dd27903",
+    "url": "/todoList/static/css/main.34658c8b.chunk.css"
+  },
+  {
+    "revision": "bac68d842d4a60bd3832",
+    "url": "/todoList/static/js/2.f0bef555.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoList/static/js/2.f0bef555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4518a8be364dd27903",
+    "url": "/todoList/static/js/main.3d97562b.chunk.js"
+  },
+  {
+    "revision": "f7f6dd64c8bfb92501a3",
+    "url": "/todoList/static/js/runtime-main.e0550b88.js"
+  }
+]);
